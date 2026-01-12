8e3ace38386316a0b3a5d26a79ff24f06766c021
youssef boualy <youssefboua913@gmail.com>
2026-01-12 14:46:35 +0100

    crée le  database et les tableau

---
+++ Db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE istichara;
+use istichara;
+
+CREATE TABLE ville (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL UNIQUE
+);
+CREATE TABLE avocat (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    ville_id INT,
+    years_of_experience INT,
+    specialite ENUM('Droit pénal', 'civil', 'famille', 'affaires') NOT NULL,
+    FOREIGN KEY (ville_id) REFERENCES ville(id)
+);
+CREATE TABLE huissier (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    ville_id INT,
+    years_of_experience INT,
+    types_actes ENUM('Signification', 'exécution', 'constats') NOT NULL,
+    FOREIGN KEY (ville_id) REFERENCES ville(id)
+);
+
