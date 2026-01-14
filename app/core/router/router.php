1202058fec0b693324b58f2c0f82e1d6e7c942a7
youssef boualy <youssefboua913@gmail.com>
2026-01-14 19:37:30 +0100

    ajouter la class router

--- app/core/router/router.php
+++ app/core/router/router.php
@@ -1,1 +1,28 @@
 <?php
+ class Router{
+    private static array $router ;
+  
+ public static function get($path,$fille){
+    self::$router[$path] = $fille;
+//     self::$router = [
+//     'dachbord' => '/../../views/admin_dashboard.php',
+//     'create' => '/../../views/create.php'
+// ];
+ }
+
+public static function dispatsh($url){
+    
+   if(array_key_exists($url,self::$router)){
+    require_once(__DIR__.self::$router[$url]);
+   }else{
+    echo "404";
+   }
+ }
+
+
+
+ 
+
+    
+}
+ 
--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 
 <?php
+require_once('../vendor/autoload.php');
 use App\Models\Avocat;
+$j = new Avocat();
+require_once('../app/core/router/router.php');
 
+Router::get('dachbord','/../../views/admin_dashboard.php');
+Router::get('create','/../../views/create.php');
+
+Router::dispatsh($_GET['url']);
