0ce680aad4aedf4b8b46a2af1f1bb58e79d47aa3
0x-DevSec <h.annane.dev@protonmail.com>
2026-01-23 19:24:59 +0100

    fix router

--- app/Helper/Response.php
+++ app/Helper/Response.php
@@ -4,8 +4,17 @@ namespace App\Helper;
 
 
 class Response {
+    
     public function header($path){
         header("Location: $path");
         exit();
     }
+    public static function json(array $data, int $status = 200): void
+{
+    http_response_code($status);
+    header('Content-Type: application/json');
+    echo json_encode($data);
+    exit;
+}
+
 }
