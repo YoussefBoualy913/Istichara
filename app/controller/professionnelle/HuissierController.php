d63471635ad54dee6a676a46b983d534cb95239f
Mohammed Bentalb <mohammedbentalb0@gmail.com>
2026-01-23 12:24:27 +0100

    Implement profile view for Avocat and create Huissier controller

--- app/controller/professionnelle/HuissierController.php
+++ app/controller/professionnelle/HuissierController.php
@@ -7,6 +7,6 @@ use App\Helper\View;
 class AvocatController{
     
     public function profile(){
-        View::render("AvocatProfile.php");
+        View::render("huissierProfile.php");
     }
 }
